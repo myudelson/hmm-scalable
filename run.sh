20b43498c593763ab3e0e2bd3785f39d8f8bdc5c
Michael Yudelson <myudelson@gmail.com>
2013-05-15 07:56:26 -0400

    updated todo and mass fit runner

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
  
-#solvers="1.2 4.2 6.2 5.2 7.2"
-solvers="1.2 4.2"
+solvers="1.2 4.2 6.2 5.2 7.2"
 flag="0"
-#cv="10,i 10,g 10,n"
-cv="10,i 10,n"
-dataset="a89"
+cv="10,i 10,g 10,n"
 #dataset="a89 b89"
-#a89
+dataset="b89"
 kcmodel="kts" # ss
-predict="1"
+predict="0"
 metrics="1"
 #delimiters="# ~" # here # is a fake delimiter that is nowhere in our data # HARDCODED
 regularization="0 4 2 1 0.5 0.25 0.125 0.0625 0.03125 0.015625"
@@ -40,13 +37,13 @@ do
                                     out_file=$d"_"$k"_out_s"$s"_f"$f"_p"$p"_m"$m"_v"$v"_c"$c"_d~.txt"
                                     if [ "$v" = "0" ]
                                     then # no cross validation
-                                        echo "./trainhmm -s "$s" -t 0.01 -q 0 -f "$f"         -p "$p" -m "$m" -c "$c" -d ~ "$train_file" "$model_file" "$predict_file" "$out_file
+                                        echo "./trainhmm -s "$s" -t 0.01 -q 0 -f "$f"         -p "$p" -m "$m" -c "$c" -d ~ "$train_file" "$model_file" "$predict_file" > "$out_file
                                         ./trainhmm -s $s -t 0.01 -q 0 -f $f       -p $p -m $m -c $c -d ~ $train_file $model_file $predict_file &> $out_file 2>&1
                                     else # with cross validation
-                                        echo "./trainhmm -s "$s" -t 0.01 -q 0 -f "$f" -v "$v" -p 1    -m "$m" -c "$c" -d ~ "$train_file" "$model_file" "$predict_file" "$out_file
-                                        ./trainhmm -s $s -t 0.01 -q 0 -f $f -v $v -p 1  -m $m -c $c -d ~ $train_file $model_file $predict_file > $out_file 2>&1
+                                        echo "./trainhmm -s "$s" -t 0.01 -q 0 -f "$f" -v "$v" -p "$p" -m "$m" -c "$c" -d ~ "$train_file" "$model_file" "$predict_file" > "$out_file
+                                        ./trainhmm -s $s -t 0.01 -q 0 -f $f -v $v -p $p  -m $m -c $c -d ~ $train_file $model_file $predict_file > $out_file 2>&1
                                     fi  
-                                    gzip -9 $predict_file
+#                                    gzip -9 $predict_file
                                     echo "..."
                                 done # cros-validate
 #                            done # delimiters
