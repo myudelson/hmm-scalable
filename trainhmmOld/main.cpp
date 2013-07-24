0514f9ebd931299035c28e04b5eee550c8351e45
Michael Yudelson <myudelson@gmail.com>
2013-07-24 14:30:22 -0400

    commit workspace features

---
+++ trainhmmOld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  trainhmmOld
+//
+//  Created by Yudelson, Michael on 6/17/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
